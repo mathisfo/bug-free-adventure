040cc3734ca611f576115fc0c7225a042bf97bf3
Mathias Fossum <mathias.s.fossum@gmail.com>
2022-11-18 15:52:22 +0100

    course route in sidebar && open activies with url (#15)

--- src/pages/courses/[...module].tsx
+++ src/pages/courses/[...module].tsx
@@ -47,7 +47,7 @@ const ModuleStatistics = () => {
 
   if (!type) {
     return (
-      <div className='m-4 grid grid-cols-2'>
+      <div className="m-4 grid grid-cols-2">
         {/* <h1 className='m-2 text-4xl'>{module![1]}</h1> */}
         <Timeline
           recommendedActivities={[
@@ -56,10 +56,28 @@ const ModuleStatistics = () => {
             ...activities.examples,
           ].filter((e) => e.sequencing > 0 && e.relatedTopic === module![1])}
         />
-        <div className="p-14 space-y-4 col-start-2">
-          <ActivityCard type="Example" bg="bg-gradient-to-r from-[#3c3b95] via-[#44439f] to-[#3c3b95] " boxColor="bg-[#4c4aa2]" fillColor="#ED3695" fillColorDark="#E54799"/>
-          <ActivityCard type="Coding" bg="bg-gradient-to-r from-[#5f80f4] via-[#6c8af3] to-[#5f80f4]" boxColor="bg-[#7795f6]" fillColor="#ED3695" fillColorDark="#6BFF93"/>
-          <ActivityCard type="Challenge" bg="bg-gradient-to-r from-[#9293cf] via-[#9a9bd0] to-[#9293cf]" boxColor="bg-[#A3a6d8]" fillColor="#ED3695" fillColorDark="#7759EB"/>
+        <div className="col-start-2 space-y-4 p-14">
+          <ActivityCard
+            type="Example"
+            bg="bg-gradient-to-r from-[#3c3b95] via-[#44439f] to-[#3c3b95] "
+            boxColor="bg-[#4c4aa2]"
+            fillColor="#ED3695"
+            fillColorDark="#E54799"
+          />
+          <ActivityCard
+            type="Coding"
+            bg="bg-gradient-to-r from-[#5f80f4] via-[#6c8af3] to-[#5f80f4]"
+            boxColor="bg-[#7795f6]"
+            fillColor="#ED3695"
+            fillColorDark="#6BFF93"
+          />
+          <ActivityCard
+            type="Challenge"
+            bg="bg-gradient-to-r from-[#9293cf] via-[#9a9bd0] to-[#9293cf]"
+            boxColor="bg-[#A3a6d8]"
+            fillColor="#ED3695"
+            fillColorDark="#7759EB"
+          />
         </div>
       </div>
     );
