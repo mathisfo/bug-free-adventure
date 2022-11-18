9686b0cd5c0f94bfbdbbeed279d5d6d446383096
johatr <johatr@stud.ntnu.no>
2022-11-18 13:03:36 +0100

    :sparkles: added topmenu to course

--- src/pages/courses/[course].tsx
+++ src/pages/courses/[course].tsx
@@ -2,6 +2,7 @@ import ContinueCard from "../../components/ContinueCard";
 import DonutChart from "../../components/DonutChart";
 import CourseStatus from "../../components/CourseStatus";
 import { useRouter } from "next/router";
+import TopMenu from "../../components/TopMenu";
 
 const Courses = () => {
   const router = useRouter();
@@ -9,8 +10,8 @@ const Courses = () => {
 
   return (
     <div>
+      <TopMenu />
       <div className="background-color col-span-4 mr-4 h-screen rounded-r-lg p-14">
-        <div className="text-color mb-8 text-3xl font-semibold">{course}</div>
         <div className="mb-24 flex flex-row">
           <div className="w-3/5 ">
             <ContinueCard
@@ -24,7 +25,7 @@ const Courses = () => {
           </div>
         </div>
         <CourseStatus />
-        <div className="background-color h-16"></div>
+        <div className="background-color h-32"></div>
       </div>
     </div>
   );
