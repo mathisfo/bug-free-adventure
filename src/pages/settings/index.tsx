5a23dc5f60874481d79f59af7c6722809e4b7b83
mathisfo <mathias.s.fossum@gmail.com>
2022-11-18 11:55:21 +0100

    kanskje en løsning shrug

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,8 +1,11 @@
+import ProgressionGrid from '../../components/ProgressionGrid';
+
 const Settings = () => {
   return (
     <div>
         <div className="background-color col-span-4 mr-4 h-full rounded-r-lg p-16 ">
           <div className="text-color mb-4 text-xl font-semibold ">Settings</div>
+          <ProgressionGrid/>
         </div>
       </div>
   );
