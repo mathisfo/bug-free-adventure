3b92a3bdfe41671a9fb6cf5b69239e1129f82966
Johanne Tronstad <43403066+johatr@users.noreply.github.com>
2023-01-05 11:51:10 +0100

    :lipstick: timeline fix (#17)
    * course route in sidebar && open activies with url (#15)
    
    * :lipstick: changed timeline from flowbite
    
    * :truck: added progressiongrid to module page
    
    Co-authored-by: Mathias Fossum <mathias.s.fossum@gmail.com>

--- src/pages/courses/[...module].tsx
+++ src/pages/courses/[...module].tsx
@@ -2,6 +2,7 @@ import { type as typeEnum } from "@prisma/client";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import ActivityCard from "../../components/ActivityCard";
+import ProgressionGrid from "../../components/ProgressionGrid";
 import Timeline from "../../components/Timeline";
 import { Activity } from "../../server/schema/LearnerActivitySchema";
 import { trpc } from "../../utils/trpc";
@@ -47,8 +48,10 @@ const ModuleStatistics = () => {
 
   if (!type) {
     return (
-      <div className='m-4 grid grid-cols-2'>
+      <div className="m-4 grid grid-cols-2 grid-rows-2 h-screen">
         {/* <h1 className='m-2 text-4xl'>{module![1]}</h1> */}
+        <div className="col-start-1 space-y-8 p-14 space-x-4">
+        <div className="text-color mb-4 text-xl font-semibold">Recommended next steps</div>
         <Timeline
           recommendedActivities={[
             ...activities.challenges,
@@ -56,10 +59,31 @@ const ModuleStatistics = () => {
             ...activities.examples,
           ].filter((e) => e.sequencing > 0 && e.relatedTopic === module![1])}
         />
-        <div className="p-14 space-y-4 col-start-2">
-          <ActivityCard type="Example" bg="bg-gradient-to-r from-[#3c3b95] via-[#44439f] to-[#3c3b95] " boxColor="bg-[#4c4aa2]" fillColor="#ED3695" fillColorDark="#E54799"/>
-          <ActivityCard type="Coding" bg="bg-gradient-to-r from-[#5f80f4] via-[#6c8af3] to-[#5f80f4]" boxColor="bg-[#7795f6]" fillColor="#ED3695" fillColorDark="#6BFF93"/>
-          <ActivityCard type="Challenge" bg="bg-gradient-to-r from-[#9293cf] via-[#9a9bd0] to-[#9293cf]" boxColor="bg-[#A3a6d8]" fillColor="#ED3695" fillColorDark="#7759EB"/>
+        </div>
+
+        <div className="row-start-2 p-12 col-start-1 col-span-2"><ProgressionGrid /></div>
+        <div className="col-start-2 space-y-4 p-14">
+          <ActivityCard
+            type="Example"
+            bg="bg-gradient-to-r from-[#3c3b95] via-[#44439f] to-[#3c3b95] "
+            boxColor="bg-[#4c4aa2]"
+            fillColor="#ED3695"
+            fillColorDark="#E54799"
+          />
+          <ActivityCard
+            type="Coding"
+            bg="bg-gradient-to-r from-[#5f80f4] via-[#6c8af3] to-[#5f80f4]"
+            boxColor="bg-[#7795f6]"
+            fillColor="#ED3695"
+            fillColorDark="#6BFF93"
+          />
+          <ActivityCard
+            type="Challenge"
+            bg="bg-gradient-to-r from-[#9293cf] via-[#9a9bd0] to-[#9293cf]"
+            boxColor="bg-[#A3a6d8]"
+            fillColor="#ED3695"
+            fillColorDark="#7759EB"
+          />
         </div>
       </div>
     );
