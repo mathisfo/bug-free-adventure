42cb02f1b76c81d423d09b3eabe4215b3d026735
mathisfo <mathias.s.fossum@gmail.com>
2023-01-23 22:33:09 +0100

    fix build error

--- src/server/api/routers/learnerActivityRouter.ts
+++ src/server/api/routers/learnerActivityRouter.ts
@@ -8,7 +8,7 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const learnerActivityRouter = createTRPCRouter({
   getLearnerActivity: protectedProcedure.query(async ({ ctx }) => {
-    const externalAPIURL = `http://adapt2.sis.pitt.edu/aggregate2/GetContentLevels?usr=${ctx.session.user.prisma?.protusId}&grp=NorwayFall2022B&mod=user&sid=TEST&cid=352&lastActivityId=while_loops.j_digits&res=-1`;
+    const externalAPIURL = `http://adapt2.sis.pitt.edu/aggregate2/GetContentLevels?usr=${ctx.session.user.protusId}&grp=NorwayFall2022B&mod=user&sid=TEST&cid=352&lastActivityId=while_loops.j_digits&res=-1`;
 
     const unfilteredAPI = await fetch(externalAPIURL)
       .then((response) => response.text())
