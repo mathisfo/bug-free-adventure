e4feb722b4d57edd900eb05df68a2c17a97243d3
johatr <johatr@stud.ntnu.no>
2023-02-17 14:49:21 +0100

    :sparkles: added achievements to profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,43 +1,64 @@
 import { useSession } from "next-auth/react";
+import { TrophyIcon } from "@heroicons/react/24/outline";
+import { api } from "../../utils/api";
 
 const Profile = () => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession({ required: true });
+
+  if (status == "loading") {
+    return <div>Loading...</div>;
+  }
+
+  const {
+    data: history,
+    isLoading,
+    isSuccess,
+  } = api.userRouter.getExerciseHistoryOnUser.useQuery({
+    userId: session.user.id,
+  });
+
+  if (isLoading || !isSuccess) {
+    return <div>Loading...</div>;
+  }
+
+  console.log("created at:" + session?.user?.createdAt?.toDateString);
 
   return (
     <div>
-      <div className="background-color col-span-4 mr-4 h-full rounded-r-lg p-16 ">
-        <div className="text-color mb-12 text-xl font-semibold uppercase opacity-75">
-          Profile
+      <div className="background-color text-color mr-4 h-full w-1/2 rounded-r-lg p-16 ">
+        <div className="mb-16 mt-12">
+          <div className="grid grid-cols-3 items-baseline border-b dark:border-zinc-700">
+            <p className=" col-start-1 text-sm font-semibold uppercase">Name</p>
+            <p className=" col-span-2 col-start-2 text-lg">
+              {session?.user?.name}
+            </p>
+          </div>
+          <div className="my-8 grid grid-cols-3 items-baseline border-b dark:border-zinc-700">
+            <p className=" col-start-1 text-sm font-semibold uppercase">
+              E-mail
+            </p>
+            <p className=" col-span-2 col-start-2 text-lg">
+              {session?.user?.USNEmail}
+            </p>
+          </div>
+          <div className="my-8 grid grid-cols-3 items-baseline border-b dark:border-zinc-700">
+            <p className=" col-start-1 text-sm font-semibold uppercase">
+              Protus ID
+            </p>
+            <p className=" col-span-2 col-start-2 text-lg">
+              {session?.user?.protusId}
+            </p>
+          </div>
         </div>
-        <div className="grid grid-cols-4 items-baseline border-b">
-          <p className="text-color col-start-1 text-sm font-semibold uppercase">
-            Name
-          </p>
-          <p className="text-color col-span-2 col-start-2 text-lg">
-            {session?.user?.name}
-          </p>
+        <div className="text-md mb-4 flex flex-row gap-3 font-semibold uppercase">
+          <TrophyIcon className="h-5 w-5 text-[#fecd66]" />
+          <p>Achievements</p>
         </div>
-        <div className="my-8 grid grid-cols-4 items-baseline border-b">
-          <p className="text-color col-start-1 text-sm font-semibold uppercase">
-            E-mail
+        <div className="my-8 grid grid-cols-3 items-baseline border-b dark:border-zinc-700">
+          <p className=" col-start-1 text-sm font-semibold uppercase">
+            Exercises done
           </p>
-          <p className="text-color col-span-2 col-start-2 text-lg">
-            {session?.user?.USNEmail}
-          </p>
-        </div>
-        <div className="my-8 grid grid-cols-4 items-baseline border-b">
-          <p className="text-color col-start-1 text-sm font-semibold uppercase">
-            Protus ID
-          </p>
-          <p className="text-color col-span-2 col-start-2 text-lg">
-            {session?.user?.protusId}
-          </p>
-        </div>
-        <div className="my-8 grid grid-cols-4 items-baseline border-b">
-          <p className="text-color col-start-1 text-sm font-semibold uppercase">
-            User since
-          </p>
-          <p className="text-color col-span-2 col-start-2 text-lg"></p>
+          <p className=" col-span-2 col-start-2 text-lg">{history.length}</p>
         </div>
       </div>
     </div>
