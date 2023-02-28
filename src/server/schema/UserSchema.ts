b65cf017af6e274180752ff08eb5994c1628cdba
mathisfo <mathias.s.fossum@gmail.com>
2023-02-28 16:10:51 +0100

    :safety_vest: preview of leaderboard && correct validation based on leaderboard optin or out

--- src/server/schema/UserSchema.ts
+++ src/server/schema/UserSchema.ts
@@ -4,24 +4,33 @@ import { z } from "zod";
 const selectedComps = z.nativeEnum(SelectedEnum);
 export type selectedCompsEnum = z.infer<typeof selectedComps>;
 
-export const onboardingSchema = z.object({
-  name: z
-    .string()
-    .min(2, { message: "Your name must be at least two characters long." })
-    .max(20, {
-      message: "The length of your name can be of maximum 20 characters.",
+export const onboardingSchema = z
+  .object({
+    name: z.string().optional(),
+    USNEmail: z.string().email().endsWith("@usn.no", {
+      message: "Your email must be an USN email.",
     }),
-  USNEmail: z.string().email().endsWith("@usn.no", {
-    message: "Your email must be an USN email.",
-  }),
-  protusId: z
-    .number({ invalid_type_error: "Your ID should be between 22101 and 22200" })
-    .gt(22100, { message: "Your ID must be greater than 22100" })
-    .lt(22201, { message: "Your ID must be lower than 22201" })
-    .int(),
-  selectedComponents: z.array(z.string()),
-  leaderboard: z.boolean(),
-});
+    protusId: z
+      .number({
+        invalid_type_error: "Your ID should be between 22101 and 22200",
+      })
+      .gt(22100, { message: "Your ID must be greater than 22100" })
+      .lt(22201, { message: "Your ID must be lower than 22201" })
+      .int(),
+    selectedComponents: z.array(z.string()),
+    leaderboard: z.boolean(),
+  })
+  .transform((data) => ({
+    ...data,
+    name: data.leaderboard ? data.name : undefined,
+  }))
+  .refine(
+    (data) => !data.leaderboard || (data.name ? data.name.length > 2 : false),
+    {
+      message: "Name must be at least three characters long",
+      path: ["name"],
+    }
+  );
 
 export const toDoSchema = z.object({
   dueDate: z.date(),
