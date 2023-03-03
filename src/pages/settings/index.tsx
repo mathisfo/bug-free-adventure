673da9593733e9a3745ae879ac1883631d16ac5a
johatr <johatr@stud.ntnu.no>
2023-03-03 18:15:09 +0100

    :lipstick: settings glow-up

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,98 +1,90 @@
 import { Checkbox } from "flowbite-react";
 import { NextPage } from "next";
+import ExerciseHistoryPreview from "../../components/previews/ExerciseHistoryPreview";
 import ExercisePlannerPreview from "../../components/previews/ExercisePlannerPreview";
+import HistoryGraphPreview from "../../components/previews/HistoryGraphPreview";
+import LeaderboardPreview from "../../components/previews/LeaderboardPreview";
+import StatsPreview from "../../components/previews/StatsPreview";
 
 const Settings: NextPage = () => {
   return (
     <div>
-      <div className="background-color col-span-4 mr-4 h-screen rounded-r-lg p-16 ">
+      <div className="background-color col-span-4 mr-4 h-screen w-11/12 rounded-r-lg p-16 ">
         <p className="text-color mb-6 text-xl font-semibold uppercase opacity-75">
           My chosen components
         </p>
-        <div>
-          <div className="mt-5 grid w-4/5 select-none grid-cols-2 gap-x-8 gap-y-4">
-            <div className="course-card relative grid grid-cols-2 gap-8 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
-              <div className="col-start-1">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                  History Graph
-                </h5>
+        <div className="mt-5 grid select-none grid-cols-2 gap-x-8 gap-y-4">
+          <div className="course-card h-auto grid-cols-1 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
+              Exercise Planner
+            </h5>
 
-                <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
-                  This component shows you how much you work per day reprented
-                  as a graph.
-                </p>
-                <div className="my-8 ml-4 flex items-center gap-2 ">
-                  <Checkbox id="select" />
-                  <label htmlFor="select">Select</label>
-                </div>
-              </div>
-              <div className="col-start-2 grid items-center ">
-                <div className=" h-32 w-32 self-center rounded bg-blue-200"></div>
-              </div>
+            <p className="text-sm text-gray-700 dark:text-gray-400">
+              This component enables you to keep track of your assignments with
+              due dates.
+            </p>
+            <div className="scale-90">
+              <ExercisePlannerPreview />
             </div>
-            <div className="course-card relative grid grid-cols-2 gap-8 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
-              <div className="col-start-1">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                  Exercise Planner
-                </h5>
+          </div>
 
-                <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
-                  This component enables you to keep track of your assignments
-                  with due dates.
-                </p>
-                <div className="my-8 ml-4 flex items-center gap-2 ">
-                  <Checkbox id="select" />
-                  <label htmlFor="select">Select</label>
-                </div>
-              </div>
-              <div className="col-start-2 grid items-center ">
-                <div className=" h-32 w-32 self-center rounded bg-blue-200"></div>
-              </div>
+          <div className="course-card rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
+              History Graph
+            </h5>
+
+            <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
+              This component shows you how much you work per day reprented as a
+              graph.
+            </p>
+            <div className="scale-90">
+              <HistoryGraphPreview />
             </div>
-            <div className="course-card relative grid grid-cols-2 gap-8 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
-              <div className="col-start-1">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                  Activity History
-                </h5>
+          </div>
+          <div className="course-card rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">Stats</h5>
 
-                <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
-                  This component is more detailed than Activity Graph. It shows
-                  your exercise activty per day, as a list.
-                </p>
-                <div className="my-8 ml-4 flex items-center gap-2 ">
-                  <Checkbox id="select" />
-                  <label htmlFor="select">Select</label>
-                </div>
-              </div>
-              <div className="col-start-2 grid items-center ">
-                <ExercisePlannerPreview />
-              </div>
+            <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
+              This component shows you some stats about the work you have put in
+              the previous week compared to the week before.
+            </p>
+            <div className="scale-90">
+              <StatsPreview />
+            </div>
+            <div className="col-start-2 grid items-center "></div>
+          </div>
+
+          <div className="course-card grid-cols-1 gap-8 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
+              Activity History
+            </h5>
+
+            <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
+              This component is more detailed than Activity Graph. It shows your
+              exercise activty per day, as a list.
+            </p>
+            <div className="scale-90">
+              <ExerciseHistoryPreview />
             </div>
           </div>
         </div>
+
         <p className="text-color mb-6 mt-8 text-xl font-semibold uppercase opacity-75">
           Leaderboard participation
         </p>
-        <div className="mt-5 grid w-4/5 select-none grid-cols-2 gap-x-8 gap-y-4">
-          <div className="course-card relative grid grid-cols-2 gap-8 rounded-2xl border border-zinc-400 px-6 pt-6 dark:border-zinc-600">
-            <div className="col-start-1">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight">
-                Leaderboard
-              </h5>
+        <div className="course-card w-3/5 rounded-2xl border border-zinc-400 pl-6 pr-2 pt-6 dark:border-zinc-600">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight">
+            Leaderboard
+          </h5>
+          <div className="grid grid-cols-2 gap-2">
+            <div className=" col-start-1 text-sm text-gray-700 dark:text-gray-400">
+              I would like to participate in the leaderboard
+            </div>
 
-              <p className="col-start-1 text-sm text-gray-700 dark:text-gray-400">
-                By participating in the leaderboard you can compete against your
-                classmates to see who completes the most exercises. Your
-                nickname will show up on the leaderboard.
-              </p>
-              <div className="my-8 ml-4 flex items-center gap-2 ">
-                <Checkbox id="select" />
-                <label htmlFor="select">Select</label>
-              </div>
+            <div className="col-start-2 scale-90">
+              <LeaderboardPreview />
             </div>
-            <div className="col-start-2 grid items-center ">
-              <div className=" h-32 w-32 self-center rounded bg-blue-200"></div>
-            </div>
+            <div className="col-start-1 row-start-2 my-6 flex items-center gap-2"></div>
           </div>
         </div>
         <div className="flex flex-row justify-end">
